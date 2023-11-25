1489a8a085177f5112e20cceb48b4a97fcb31d6d
mack <ymes60404@gmail.com>
2023-11-25 17:10:28 +0800

    init

---
+++ src/test/java/com/example/stu_club/StuClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stu_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
